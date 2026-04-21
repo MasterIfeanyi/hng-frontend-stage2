ebc5091e6b034093b619e3ac08141ef47278cc2a
Ifeanyi Chima <chimaifeanyi29@gmail.com>
2026-04-21 08:43:24 +0100

    chore: initial project setup with create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Invoice App - Under Construction 🚧</h1>
+      <p>Checkpoint 0 Complete!</p>
+    </>
+  )
+}
+
+export default App
